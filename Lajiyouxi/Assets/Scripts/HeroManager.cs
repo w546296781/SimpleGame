74ef880db7563996f372985c6cf1b6c1f994e395
Xinkai Wang <546296781@qq.com>
2020-05-20 14:00:40 -0700

    Initial

---
+++ Lajiyouxi/Assets/Scripts/HeroManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroManager : MonoBehaviour
+{
+    public int health, atk, def;
+    public bool isLive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 100;
+        isLive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(health <= 0)
+        {
+            isLive = false;
+        }
+    }
+}
