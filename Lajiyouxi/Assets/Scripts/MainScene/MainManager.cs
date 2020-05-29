b1bb25b090959a5e6da6637b0edf9bfaaf9f3def
Xinkai Wang <546296781@qq.com>
2020-05-29 10:47:49 -0700

    Add prefab for each type of events

--- Lajiyouxi/Assets/Scripts/MainScene/MainManager.cs
+++ Lajiyouxi/Assets/Scripts/MainScene/MainManager.cs
@@ -7,10 +7,14 @@ public class MainManager : MonoBehaviour
 {
     public GameObject setting_popup;
 
+    private Vector3 position_area1 = new Vector3(-550, 20, 0);
+    private Vector3 position_area2 = new Vector3(-0, 20, 0);
+    private Vector3 position_area3 = new Vector3(550, 20, 0);
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //在3个位置随机分配3张卡
     }
 
     // Update is called once per frame
@@ -28,5 +32,20 @@ public class MainManager : MonoBehaviour
     {
         GameObject instance = (GameObject)Instantiate(setting_popup, transform.position, transform.rotation);
         instance.transform.parent = transform;
+    }
+
+    public void TreasureEvent()
+    {
+
+    }
+
+    public void ShopEvent()
+    {
+
+    }
+
+    public void AdventureEvent()
+    {
+
     }
 }
